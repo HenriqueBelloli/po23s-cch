49937f005fc4fb1788570c34e608480c72bbd894
HenriqueBelloli <belloli.henrique@gmail.com>
2022-06-16 04:12:48 -0300

    criado primeira tela

--- src/main/java/utfpr/banco/Banco.java
+++ src/main/java/utfpr/banco/Banco.java
@@ -1,45 +1,33 @@
 package utfpr.banco;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Banco {
+
     private String nome;
     private ArrayList<ContaBancaria> contas;
-    
-    public Banco(String nome){
+
+    public Banco(String nome) {
         this.nome = nome;
         this.contas = new ArrayList<>();
     }
-    
-    public void adicionar(ContaBancaria novaConta){
-        if (novaConta.getNumero() == 0){
+
+    public void adicionar(ContaBancaria novaConta) {
+        if (novaConta.getNumero() == 0) {
             System.out.println("Erro: conta tem que ter um número!");
         } else {
             this.contas.add(novaConta);
         }
     }
-    
-    public ContaBancaria getConta(int numero){
-        for (ContaBancaria conta : contas){
-            if (conta.getNumero() == numero){
+
+    public ContaBancaria getConta(int numero) {
+        for (ContaBancaria conta : contas) {
+            if (conta.getNumero() == numero) {
                 return conta;
             }
         }
         return null;
-    }
-    
-    // IMPLEMENTAR...
-    // remover
-    // saldo de contas poupança
-    // saldo de contas corrente
-    // saldo de todas contas
-    
-    public void listarContas(){
-        System.out.println("Banco: " + this.nome);
-        System.out.println("Lista de contas...");
-        for (ContaBancaria conta : contas){
-            System.out.println(conta.getInformacoes());
-        }
     }
 
     public String getNome() {
@@ -50,7 +38,105 @@ public class Banco {
         this.nome = nome;
     }
 
-    
-    
-    
+    // IMPLEMENTAR...
+    // remover
+    // saldo de contas poupança
+    // saldo de contas corrente
+    // saldo de todas contas
+    public void listarContas() {
+        System.out.println("Banco: " + this.nome);
+        System.out.println("Lista de contas...");
+        for (ContaBancaria conta : contas) {
+            System.out.println(conta.getInformacoes());
+        }
+    }
+
+    public ContaBancaria getContaMaiorSaldo() {
+        ContaBancaria contaRetorno = null;
+
+        for (ContaBancaria conta : contas) {
+
+            if (contaRetorno == null || conta.getSaldo() > contaRetorno.getSaldo()) {
+                contaRetorno = conta;
+            }
+        }
+        return contaRetorno;
+    }
+
+    public ContaBancaria getContaMenorSaldo() {
+        ContaBancaria contaRetorno = null;
+
+        for (ContaBancaria conta : contas) {
+
+            if (contaRetorno == null || conta.getSaldo() < contaRetorno.getSaldo()) {
+                contaRetorno = conta;
+            }
+        }
+        return contaRetorno;
+    }
+
+    public double getSaldoMedioContas() {
+        double saldoMedio = 0;
+
+        for (ContaBancaria conta : contas) {
+            saldoMedio += conta.getSaldo();
+        }
+
+        if (!contas.isEmpty()) {
+            saldoMedio = saldoMedio / contas.size();
+        }
+
+        return saldoMedio;
+    }
+
+    public double getSaldoMedioContasCorrente() {
+        double saldoMedio = 0;
+        Integer quantidade = 0;
+
+        for (ContaBancaria conta : contas) {
+            if (conta instanceof ContaCorrente) {
+                quantidade++;
+                saldoMedio += conta.getSaldo();
+            }
+        }
+
+        if (quantidade > 0) {
+            saldoMedio = saldoMedio / quantidade;
+        }
+
+        return saldoMedio;
+    }
+
+    public double getSaldoMedioContasPupanca() {
+        double saldoMedio = 0;
+        Integer quantidade = 0;
+
+        for (ContaBancaria conta : contas) {
+            if (conta instanceof ContaPoupanca) {
+                quantidade++;
+                saldoMedio += conta.getSaldo();
+            }
+        }
+
+        if (quantidade > 0) {
+            saldoMedio = saldoMedio / quantidade;
+        }
+
+        return saldoMedio;
+    }
+
+    public HashMap listarClientes() {
+
+        HashMap<Integer, String> clientes = new HashMap();
+
+        for (ContaBancaria conta : contas) {
+            Cliente titular = conta.getTitular();
+
+            if (!clientes.containsKey(titular.getCod())) {
+                clientes.put(titular.getCod(), titular.getNome());
+            }
+        }
+
+        return clientes;
+    }
 }
--- src/main/java/utfpr/util/Main.java
+++ src/main/java/utfpr/util/Main.java
@@ -1,20 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utfpr.util;
 
 import utfpr.banco.Banco;
+import utfpr.gui.TelaPrincipal;
 
-/**
- *
- * @author evand
- */
 public class Main {
     public static void main(String[] args) {
         DataProvider provider = new DataProvider();
         Banco banco = provider.criarBanco("Caixa");
         banco.listarContas();
+        banco.listarClientes();
+        
+        TelaPrincipal objPrincipal = new TelaPrincipal();
+        objPrincipal.setBanco(banco);
+        objPrincipal.setVisible(true);
     }
 }
